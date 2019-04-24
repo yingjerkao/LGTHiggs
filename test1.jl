744bfbbd23b6e1e7241698e4d5fe0faff3d0c9ce
Ying-Jer Kao <yingjerkao@gmail.com>
2019-04-24 09:05:50 -0400

    Added wilsonloop test

--- test1.jl
+++ test1.jl
@@ -7,3 +7,17 @@ A=rand(GaugeFieldSU2,latt)
 
 heatbath_gauge!(A,10.0,5)
 overrelaxation_gauge!(A,10.0)
+
+function test_wislonloop()
+    result=0.0
+    count=0
+    for μ in 1:3, ν in 1:3
+    if μ != ν
+        count+=1
+        result+=plaquette(A,μ,ν,CartesianIndex(1,1,1))
+    end
+    end
+    println(result/count)
+    println(wilsonloop(A,CartesianIndex(1,1,1),1,1))
+end
+test_wislonloop()
